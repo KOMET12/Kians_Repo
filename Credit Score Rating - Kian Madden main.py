f77e79a222bc2f68907b5521e8d7201a65007938
KOMET12 <142356636+KOMET12@users.noreply.github.com>
2023-09-06 15:49:17 -0500

    Add files via upload

---
+++ Credit Score Rating - Kian Madden main.py
@@ -0,0 +1,8 @@
+Credit_Score = int(input("What is your credit score?"))
+
+if Credit_Score <= 689:
+    print("Piss poor credit score")
+elif Credit_Score >= 690:
+    print("Decent credit score")
+elif Credit_Score >= 720:
+    print("Excellent credit score")
