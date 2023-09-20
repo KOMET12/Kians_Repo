8c68ed7a2860e12f92d902e2835c2127d6a071b7
KOMET12 <142356636+KOMET12@users.noreply.github.com>
2023-09-20 15:54:49 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-student_ticket = 5.00
 
-verteran_ticket = 7.00
-
-sponsor_ticket = 2.00
-
-retiree_ticket = 6.00
-
-public_ticket = 10.00
-
-print("1.student_ticket = 5.00")
-print("2.verteran_ticket = 7.00")
-print("3.sponsor_ticket = 2.00")
-print("4.retiree_ticket = 6.00")
-print("5.public_ticket = 10.00")
-
-Ticket_type = (input("What category of tickets would you like? "))
-Total_tickets = (int(input("How many tickets? "))
-
-Print()
+count = 99
+while (count>=):
+    if count == 2:
+        print(f"{count} bottles of beer on the wall")
+        print(f"{count} bottles of beer")
+        print(f"Take one down, pass it around")
+        print(f"{count - 1} bottle of beer on the wall\n")
+    elif count == 1:
+        print(f"{count} bottle of beer on the wall")
+        print(f"{count} bottle of beer")
+        print(f"Take one down, pass it around")
+        print(f"{count - 1} bottles of beer on the wall\n")
+    else:
+        print(f"{count} bottles of beer on the wall")
+        print(f"{count} bottles of beer")
+        print(f"Take one down, pass it around")
+        print(f"{count - 1} bottles of beer on the wall\n")
+    count = count - 1
